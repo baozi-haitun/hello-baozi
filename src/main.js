41bc639b81d7daa25e9b4a62f5f737fd9f8a0591
龚子芹 <gongziqin@gongziqindeMacBook-Pro.local>
2021-06-28 09:46:09 +0800

    vue框架制作掌云

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'  //import 变量 from 组件位置
-import App from './App.vue'
 import "./assets/css/base.css"//vs ??
-import VueRouter from 'vue - router'
+import VueRouter from 'vue-router'
 
 
 Vue.config.productionTip = false  //???
 Vue.use(VueRouter)
-import Detail from './components/Detail.vue'
+import Detail from './view/Detail.vue'
+import Index from './view/index.vue'
+import App from './App.vue'
+
 
 var routes = [
+  { path: '/', component: Index },
   { path: '/detail', component: Detail }
 ]
 
@@ -16,6 +19,8 @@ const router = new VueRouter({
   mode: "hash",
   routes
 })
+
+
 new Vue({
   router,
   render: h => h(App),
